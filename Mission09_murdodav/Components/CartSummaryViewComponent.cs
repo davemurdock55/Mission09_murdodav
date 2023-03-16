a54da065ed5fd4201a8d09c3795ecd1e8c593aab
David Murdock <davemurdock55@gmail.com>
2023-03-16 00:29:59 -0600

    Finished!!!

---
+++ Mission09_murdodav/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_murdodav.Models;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart temp_cart)
+        {
+            cart = temp_cart;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
